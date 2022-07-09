0db3236b8fcdc80a9c4b7d0ff4fff28f32dda0a8
wuhao007 <tohaowu@gmail.com>
2022-07-09 16:13:18 -0700

    Delete folders

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 # your local path to API keys and logs folder
-path_key = '/Users/sunny/trading_bot_example/other/'
-path_logs = '/Users/sunny/trading_bot_example/logs/'
+path_key = '/Users/sunny/key/'
+path_logs = '/tmp/'
 
 # sell_levels and buy_levels are lists of lists. It defines:
 # [ [ percented of balance to trade, price level, unique userref ], [ ... ] ]
-sell_levels = []
-buy_levels = [(0.5, 0.992, 201), (0.3, 0.996, 202), (0.2, 0.998, 203)]
+buy_levels = [0.5]
 
 # pairs is a list if list that defines tradable pairs and assets:
 # [ [ base asset, quote asset, altname from AssetPairs endpoint ], [ ... ] ]
